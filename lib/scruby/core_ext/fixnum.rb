24c0d2d183f64930bcb87be8b3fb82c6d62376bb
Damián Silvani <munshkr@gmail.com>
2018-09-13 00:17:28 -0300

    Remove Fixnum warning on core extension

--- lib/scruby/core_ext/fixnum.rb
+++ lib/scruby/core_ext/fixnum.rb
@@ -1,8 +1,21 @@
 # Musical math
-class Fixnum  
-  def freq
-    440 * (2 ** ((self - 69) * 0.083333333333) )
+module Scruby::CoreExt
+  module Fixnum
+    def freq
+      440 * (2 ** ((self - 69) * 0.083333333333))
+    end
+
+    #method next tone
+    # 1:1.05946
   end
-  #method next tone
-  # 1:1.05946
+end
+
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.4")
+  class Fixnum
+    prepend Scruby::CoreExt::Fixnum
+  end
+else
+  class Integer
+    prepend Scruby::CoreExt::Fixnum
+  end
 end
